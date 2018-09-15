43b78a9bd1ac0ee228cd38b13519d221619d72f5
kehtolaulu <emilia270627@gmail.com>
2018-09-15 22:05:44 +0300

    added task 1.11

---
+++ task-1.11.rkt
@@ -0,0 +1,17 @@
+; this is a recursive process
+
+(define (f n)
+  (cond ((< n 3) n)
+        (else (+ (f (- n 1))
+                 (f (- n 2))
+                 (f (- n 3))))))
+
+; this is an iterative process
+
+(define (f n)
+  (f-iter 2 1 0 n))
+
+(define (f-iter a b c count)
+  (if (= count 0)
+      c
+      (f-iter (+ a b c) a b (- count 1))))
